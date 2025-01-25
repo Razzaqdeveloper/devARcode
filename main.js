41bcbff9a5d98dd74ea7d3cd4fadab00c96fd779
Abdur Razzaq <104042202+Razzaqdeveloper@users.noreply.github.com>
2025-01-25 08:45:45 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+document.getElementById('mobile-menu-button').addEventListener('click', function () {
+  const menu = document.getElementById('mobile-menu');
+  menu.classList.toggle('hidden');
+});
+
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
+
+
+const toggleResume = () => {
+  const cartModal = document.getElementById("cartmodal")
+  cartModal.classList.toggle("-translate-y-full")
+}
